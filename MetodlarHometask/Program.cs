2ecfc3aecbc1efcf7ed85171aa5a40efbd4d544a
TunarN <tunarzn@code.edu.az>
2022-08-07 14:58:34 +0400

    task4

---
+++ MetodlarHometask/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace MetodlarHometask
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = { 1, 2, 3, 4, 5 };
+            int num = 4;
+            Console.WriteLine(FoundNum(nums,num));
+        }
+        static bool FoundNum(int[] nums, int num)
+        {
+            for (int i = 0; i <nums.Length; i++)
+            {
+                if (nums[i]==num)
+                {
+                    return true;
+                }
+               
+            }
+            return false;
+        }
+    }
+}
